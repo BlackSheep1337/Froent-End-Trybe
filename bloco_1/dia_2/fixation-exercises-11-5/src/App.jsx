48f680210bc1e24981252044aa78910ca786e935
Alexandre <naitwa.alexandre@gmail.com>
2021-05-26 09:05:43 -0300

    some changes

--- bloco_1/dia_2/fixation-exercises-11-5/src/App.jsx
+++ bloco_1/dia_2/fixation-exercises-11-5/src/App.jsx
@@ -1,28 +1,10 @@
 import React from 'react';
 import './App.css';
 import Order from './components/Order';
+import { headphone, energyDrink } from './components/Products';
 
 class App extends React.Component {
   render() {
-    const headphone = {
-      id: 102,
-      user: 'cena@gmail.com',
-      product: 'Razer Headphone',
-      price: {
-        value: 99.99,
-        currency: 'dollars'
-      }
-    };
-
-    const energyDrink = {
-      id: 77,
-      user: 'cena@gmail.com',
-      product: 'Monster 500ML',
-      price: {
-        value: 9.99,
-        currency: 'dollars'
-      }
-    };
 
     return (
       <div className="App">
---
+++ bloco_1/dia_2/fixation-exercises-11-5/src/components/Products.jsx
@@ -0,0 +1,21 @@
+const headphone = {
+  id: 102,
+  user: 'cena@gmail.com',
+  product: 'Razer Headphone',
+  price: {
+    value: 99.99,
+    currency: 'dollars'
+  }
+};
+
+const energyDrink = {
+  id: 77,
+  user: 'cena@gmail.com',
+  product: 'Monster 500ML',
+  price: {
+    value: 9.99,
+    currency: 'dollars'
+  }
+};
+
+export { headphone, energyDrink };
