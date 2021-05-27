5031b39ea6f97107a5fdd256f156062aa54bb5ad
Alexandre <naitwa.alexandre@gmail.com>
2021-05-27 17:15:30 -0300

    super exemple

--- bloco_2/dia_1/fancy-button/src/App.jsx
+++ bloco_2/dia_1/fancy-button/src/App.jsx
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import  { Component } from 'react';
 import './App.css';
 
-function handleClick(e) {
-  const btn = e.target.innerText;
-  btn === 'Vermelho' ? console.log('Vermelho') :
-  btn === 'Verde' ? console.log('Verde') : console.log('Azul');
-}
+
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+    console.log('Componente Sendo Construido');
+  }
+
+  handleClick(e) {
+    console.log(this);
+    const btn = e.target.innerText;
+    btn === 'Vermelho' ? console.log('Vermelho') :
+    btn === 'Verde' ? console.log('Verde') : console.log('Azul');
+  }
+
   render() {
+    console.log(this);
     return (
       <div className="containerApp">
-      <button className="button red" onClick={handleClick}>Vermelho</button>
-      <button className="button green" onClick={handleClick}>Verde</button>
-      <button className="button blue" onClick={handleClick}>Azul</button>
+      <button className="button red" onClick={this.handleClick}>Vermelho</button>
+      <button className="button green" onClick={this.handleClick}>Verde</button>
+      <button className="button blue" onClick={this.handleClick}>Azul</button>
     </div>
     );
   }
 }
-
