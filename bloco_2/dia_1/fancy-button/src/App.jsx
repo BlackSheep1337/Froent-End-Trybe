3088091cd6588068f63b09449a0e37faa7f42ca4
Alexandre <naitwa.alexandre@gmail.com>
2021-05-27 17:50:07 -0300

    trocar estados

--- bloco_2/dia_1/fancy-button/src/App.jsx
+++ bloco_2/dia_1/fancy-button/src/App.jsx
@@ -6,9 +6,15 @@ import './App.css';
 export default class App extends Component {
   constructor() {
     super();
+    this.state = {
+      count: 0,
+      background: 'red',
+      color: 'white',
+    }
     this.handleClick = this.handleClick.bind(this);
     console.log('Componente Sendo Construido');
   }
+
 
   handleClick(e) {
     console.log(this);
@@ -21,6 +27,9 @@ export default class App extends Component {
     console.log(this);
     return (
       <div className="containerApp">
+      <div className="counter" style={{background: this.state.background, color: this.state.color}} >
+        <div className="count">{this.state.count}</div>
+      </div>
       <button className="button red" onClick={this.handleClick}>Vermelho</button>
       <button className="button green" onClick={this.handleClick}>Verde</button>
       <button className="button blue" onClick={this.handleClick}>Azul</button>
