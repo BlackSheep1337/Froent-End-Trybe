8cef9dd9ff73c53dc9af4d4c13906f1f8e2965cf
Alexandre <naitwa.alexandre@gmail.com>
2021-06-02 16:31:56 -0300

    daddy jokes

---
+++ bloco_3/dia_1/daddy-jokes/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import DadJoke from './DadJoke'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <DadJoke />
+      </div>
+    )
+  }
+}
